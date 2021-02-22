81086d8695d8f633b39686a7b7909174473bba42
jordi <jordicalatayudcastro@gmail.com>
2021-02-22 15:22:55 +0100

    Commit revisat Ex3 i nivell2

--- js/programa.js
+++ js/programa.js
@@ -32,19 +32,34 @@ console.log("....................................................");
 
 
 // Emmagatzemar en un map les lletres repetides del meu nóm//
-var arryNom = ["j", "o", "r", "d", "i", "j"];
-var charMap = arryNom.map(function () {
-    var myMap = {};
-    for (let index of arryNom) {
-        if (myMap[index] == undefined) {
-            myMap[index] = 0;
-        }
-        myMap[index]++;
-    }
-    return myMap;
 
+var arryNom = ["j", "o", "r", "d", "i", "j"];
+var listedCounts = {};
+
+var newMap = arryNom.map(char => {
+    if (!listedCounts[char]) {
+        listedCounts[char] = 1;
+    } else {
+        listedCounts[char]++;
+    }
+    return listedCounts;
 });
-console.log(charMap[0]);
+console.log(newMap[0]);
+
+//Primer intent del Ex!
+
+// var charMap = arryNom.map(function () {
+//     var myMap = {};
+//     for (let index of arryNom) {
+//         if (myMap[index] == undefined) {
+//             myMap[index] = 0;
+//         }
+//         myMap[index]++;
+//     }
+//     return myMap;
+
+// });
+// console.log(charMap[0]);
 
 
 console.log("....................................................");
@@ -57,33 +72,20 @@ let nombreCompleto = nombre.concat([" "], apellido);
 
 console.log(nombreCompleto);
 
-
+//Ex nivell 2
 console.log(".....................Nivell-2........................");
 
 var StrObj = 'Una dirección de corr nombre-de-usuario@ionos.es: La parte del dominio depende del dominio bajo el cual se crea la dirección de correo electrónico: en nuestro ejemplo es ionos.es. Esta información varía de proveedor a proveedor, por lo que una parte del dominio también puede ser gmail.com o gmx.es si utiliza una dirección de correo electrónico de estos proveedores. Si ha registrado su propio dominio, por ejemplo, www.el-nombre-de-sus-sueños.es, las direcciones de correo electrónico que configura para el dominio lo tienen como parte del dominio (nombre-de-usuario@el-nombre-de-sus-sueños.es o nombre-de-usuario@el-nombre-de-sus-sueños.ES). El nombre de usuario es la parte de una dirección de correo electrónico que puede seleccionar libremente en la medida de lo posible. Puede, por ejemplo, utilizar su propio nombre o el nombre o departamento de una empresa. Si utiliza una dirección de correo electrónico con un proveedor de correo como gmail.com o gmx.es, es posible que la combinación con la parte del dominio deseada ya esté registrada. En este caso, deberá considerar alternativas para el nombre de usuario de su dirección de correo electrónico. Si utiliza su propio dominio, estas restricciones no se aplican porque sólo usted puede crear direcciones de correo electrónico que coincidan con su propio dominio. En resumen, nombre-de-usuario@ionos.es es un email';
 
-var emailsArray = StrObj.match(/\w*-\w*-\w*@\w*-\w*-\w*-\w*-\w*...\.(es|ES)/);
-
-var emailsArray1 = StrObj.match(/\w*-\w*-\w*@\w*\.(es)/gi);
+var regex = /\w*-?\w*-[a-z]+@[a-z-]+(\w...)\.([a-zA-Z]+)/g;
+var found = StrObj.match(regex);
 
 function findEmailAddresses(StrObj) {
-    var email = ["none"]; // Si hay match imprimir esto
-    // var emailsArray = StrObj.match(/\w*-\w*-\w*@\w*-\w*-\w*-\w*-\w*...\.(es|ES)/gi);
-    // var emailsArray1 = StrObj.match(/\w*-\w*-\w*@\w*\.(es)/gi);
-
-    if (emailsArray) {
-        var email = "";
-        for (var i = 0; i < emailsArray.length; i++) {
-            for (var i = 0; i < emailsArray1.length; i++) {
-                if (i != 0) {
-
-                    email += emailsArray1[i] + "\n";
-                }
-                email += emailsArray[i] + "\n";
-            }
-        }
-
-    }
-    return email;
+    return found.slice(0, 3);
 }
-console.log(emailsArray1);
+console.log(found[0]);
+console.log(found[1]);
+console.log(found[2]);
+
+console.log(findEmailAddresses());
+
